4bad289da116f19a0c650f114974b30862a9b4bc
Rifatinfo <mdrifathossainsinfo@gmail.com>
2026-05-19 14:51:30 +0600

    add shipmemnt

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,40 +1,43 @@
-import express from 'express';
-import { UserRoutes } from '../modules/user/user.routes';
-import { ProductRoutes } from '../modules/product/product.routes';
-import { AuthRoutes } from '../modules/auth/auth.routes';
-import { apiLimiter } from '../middlewares/rateLimiter';
-import { OrderRoutes } from '../modules/order/order.routes';
-import { AdminRoutes } from '../modules/admin/admin.routes';
-
+import express from "express";
+import { UserRoutes } from "../modules/user/user.routes";
+import { ProductRoutes } from "../modules/product/product.routes";
+import { AuthRoutes } from "../modules/auth/auth.routes";
+import { apiLimiter } from "../middlewares/rateLimiter";
+import { OrderRoutes } from "../modules/order/order.routes";
+import { AdminRoutes } from "../modules/admin/admin.routes";
+import { OrderShipment } from "../modules/shipment/shipment.route";
 
 const router = express.Router();
 
 router.use(apiLimiter);
 
 const moduleRoutes = [
-    
-    {
-        path: '/user',
-        route: UserRoutes
-    },
-    {
-        path: '/product',
-        route: ProductRoutes
-    },
-    {
-        path: '/auth',
-        route: AuthRoutes
-    },
-    {
-        path: '/order',
-        route: OrderRoutes
-    },
-    {
-        path : "/admin",
-        route : AdminRoutes
-    },
+  {
+    path: "/user",
+    route: UserRoutes,
+  },
+  {
+    path: "/product",
+    route: ProductRoutes,
+  },
+  {
+    path: "/auth",
+    route: AuthRoutes,
+  },
+  {
+    path: "/order",
+    route: OrderRoutes,
+  },
+  {
+    path: "/shipment",
+    route: OrderShipment,
+  },
+  {
+    path: "/admin",
+    route: AdminRoutes,
+  },
 ];
 
-moduleRoutes.forEach(route => router.use(route.path, route.route))
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
